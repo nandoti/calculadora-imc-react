9e74f0de875be9afa15375a6e5f5ddf27af23dc9
nandoti <fernando.adriano.tavares@gmail.com>
2022-02-17 22:18:59 -0300

    :construction: Calculadora IMC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { useState } from "react";
+
+export default function App() {
+  const [peso, setPego] = useState("");
+  const [altura, setAltura] = useState("");
+  const [mensagem, setMensagem] = useState("");
+
+  const calcularImc = () => {
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if (imc < 18.6) {
+      setMensagem("Você está abaixo do peso: " + imc.toFixed(2));
+    } else if (imc >= 18.6 && imc < 24.9) {
+      setMensagem("Peso ideal! Seu IMC: " + imc.toFixed(2));
+    } else if (imc >= 24.9 && imc < 34.9) {
+      setMensagem(
+        "Você está levemente acima do peso! Seu IMC" + imc.toFixed(2)
+      );
+    } else if (imc > 34.9) {
+      setMensagem("Cuidado Obesidade! Seu IMC: " + imc.toFixed(2));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Calculador IMC</h1>
+      <span>Vamo calcular seu IMC</span>
+      <div className="area-input">
+        <input
+          type="text"
+          name=""
+          placeholder="Peso em (kg) Ex: 90"
+          value={peso}
+          onChange={(ev) => setPego(ev.target.value)}
+        />
+        <input
+          type="text"
+          name=""
+          placeholder="Altura em (cm) Ex: 180"
+          value={altura}
+          onChange={(ev) => setAltura(ev.target.value)}
+        />
+        <button onClick={calcularImc}>Calcular</button>
+      </div>
+      <h2>{mensagem}</h2>
     </div>
   );
 }
-
-export default App;
